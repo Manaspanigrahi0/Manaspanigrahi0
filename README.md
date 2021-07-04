5ac4fc540f7840dd28a88d01ddb9328ec7526e0a
Manaspanigrahi0 <62853388+Manaspanigrahi0@users.noreply.github.com>
2021-07-04 23:25:37 +0530

    Update and rename README.md to hackerearth/splithouse.py

--- README.md
+++
@@ -1,1 +0,0 @@
-# Manaspanigrahi0
---
+++ hackerearth/splithouse.py
@@ -0,0 +1,10 @@
+# Manaspanigrahi0
+n= int(input())
+s= input()
+m=""
+if "HH" in s:
+     print("NO")
+else:
+    m= s.replace(".","B")
+    print("YES")
+    print(m)
